6ac2604d0192222817d0512d640c30efaa255662
Archemiy <60168384+Archemiy@users.noreply.github.com>
2021-02-17 09:41:37 +1000

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,24 @@
+#include  <iostream>
+using namespace std;
+
+class Human {
+public:
+	size_t high, weigth;
+	string country;
+
+	string askName() {
+		return this->Name;
+	}
+
+	size_t askAge() {
+		return this->age;
+	}
+
+private:
+	string Name, Last_name;
+	size_t age;
+};
+
+int main() {
+	
+}
